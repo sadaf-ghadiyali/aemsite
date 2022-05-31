38f9b6d3b2e5e0f2a3a65168945783ca9649e4b1
sadafghadiyali <sadaf.ghadiyali@ugamsolutions.com>
2022-05-31 21:05:53 +0530

    Multiple Exporters

--- core/src/main/java/com/aemsite/core/models/impl/AuthorBooksImpl.java
+++ core/src/main/java/com/aemsite/core/models/impl/AuthorBooksImpl.java
@@ -6,10 +6,7 @@ import com.aemsite.core.models.AuthorBooks;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Exporter;
-import org.apache.sling.models.annotations.ExporterOption;
-import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.*;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.slf4j.Logger;
@@ -25,7 +22,11 @@ import java.util.*;
         adapters = AuthorBooks.class,
         resourceType = AuthorBooksImpl.RESOURCE_TYPE,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-@Exporter(name="xmlexporter", extensions = "xml")
+@Exporters({
+        @Exporter(name="xmlexporter", extensions = "xml"),
+        @Exporter(name="jackson",extensions = "json")
+})
+
 @XmlRootElement(name = "demosite-exporter") //this is mandatory in xml exporter
 public class AuthorBooksImpl implements AuthorBooks {
 
