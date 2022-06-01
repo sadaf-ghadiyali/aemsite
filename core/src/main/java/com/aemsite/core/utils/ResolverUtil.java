d50318dffc22d9b79b676343abc6d6634b1193c3
sadafghadiyali <sadaf.ghadiyali@ugamsolutions.com>
2022-06-01 19:18:33 +0530

    System User
    They have minimum permisssions and are usually mapped to a particular on which they will work. There are two ways of creating system user:
    1.From Explorer:
     Go to localhost:4502/crx/explorer,
    login by admin
     user administration -> create System user , Intermediate Path = system . Add permissions to this user from useradmin
    2.From ACS Commons:
    Go to configMgr, search for ensure service User
    Add userid , add permissions. Jcr:read –  Eg: demosite-service-user
    THEN
    Map the user using service Apache Sling Service User Mapper Service Amendment
    Bundlename:serviceusername=userid Eg: demosite.core:demositeserviceuser=demosite-service-user
    
    Then to access the resolver for this user, write the below ResolverUtil.
    
    ResourceResolverFactory can be accessed by @Reference annotation

---
+++ core/src/main/java/com/aemsite/core/utils/ResolverUtil.java
@@ -0,0 +1,22 @@
+package com.aemsite.core.utils;
+
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResolverUtil {
+    ResolverUtil(){}
+
+    public static final String AEMSITE_SERVICE_USER = "aemsiteserviceuser";
+
+    public static ResourceResolver newResolver(ResourceResolverFactory resourceResolverFactory) throws LoginException {
+        final Map<String,Object> paramMap= new HashMap<>();
+        paramMap.put(ResourceResolverFactory.SUBSERVICE,AEMSITE_SERVICE_USER);
+        //fetches the admin service resolver using service user
+        ResourceResolver resourceResolver=resourceResolverFactory.getResourceResolver(paramMap);
+        return resourceResolver;
+    }
+}
